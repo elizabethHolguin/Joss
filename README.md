7f5b5e9b3ea4f61f90167ff6e86be2744df4336d
elizabethHolguin <jeholgui@espol.edu.ec>
2020-07-13 19:55:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-71mgl9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-71mgl9)
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  navbarOpen: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  toggleNavbar(){
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+}
